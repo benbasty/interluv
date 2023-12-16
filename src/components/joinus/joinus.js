cebf796c1246f4eeba7b35e070390ea7e3123203
benbasty <benbasty@yahoo.fr>
2023-12-16 09:29:22 +0800

    Join Us Form

---
+++ src/components/joinus/joinus.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Joinus = () => {
+  return (
+    <section id='joinus'>
+        <div className='newsletter'>
+            <h1>Join us on newsletter</h1>
+            <p>
+                Interlove lets you easily stay in touch by updating you with what's going on recently. You can enter with your email and subscribe to our newsletter and lets get the fun started.
+            </p>
+        </div>
+        <div className='email'>
+            <input type='email'/>
+            <button>Send</button>
+        </div>
+    </section>
+  )
+}
+
+export default Joinus
