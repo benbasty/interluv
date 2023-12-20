81c7ca789f7a895e8a390dc54c535b0bd0975eda
benbasty <benbasty@yahoo.fr>
2023-12-21 00:10:29 +0800

    booking form part 1

--- src/components/form/bookingform.js
+++ src/components/form/bookingform.js
@@ -4,18 +4,24 @@ import bookingFormImg from '../../assets/images/bookingfrm-nobg.png'
 
 const Bookingform = () => {
   return (
-    <section id='bookingform'>
-        <div className='container booking_frm_container'>
-            <div className='booking_form_desc'>
-                <h1>Book A Consultation</h1>
-                <p>Don't hesitate to drop us a line.</p>
-                <p>Let's make some magic togeether.</p>
+    <>
+        <section id='bookingform'>
+            <div className='container booking_frm_container'>
+                <div className='booking_form_desc'>
+                    <h1>Book A Consultation</h1>
+                    <p>Don't hesitate to drop us a line.</p>
+                    <p>Let's make some magic togeether.</p>
+                </div>
+                <div className='booking_form_img'>
+                    <img src={bookingFormImg} alt='bkform'/>
+                </div>
             </div>
-            <div className='booking_form_img'>
-                <img src={bookingFormImg} alt='bkform'/>
-            </div>
-        </div>
-    </section>
+        </section>
+        <section id='emptyspace'>
+
+        </section>
+    </>
+
   )
 }
 
